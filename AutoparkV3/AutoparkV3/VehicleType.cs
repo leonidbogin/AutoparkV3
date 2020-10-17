5c48d747115beae69fb47433b206793513ecbc0f
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-10-17 14:49:38 +0300

    Create class VehicleType

---
+++ AutoparkV3/AutoparkV3/VehicleType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoparkV3
+{
+    public class VehicleType
+    {
+        public string TypeName { get; set; }
+    }
+}
